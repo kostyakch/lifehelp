655d27a8747aa681f4d783b59198704e46c720ce
Konstantin Chernyaev <kch@list.ru>
2020-05-19 16:39:41 +0300

    Fix Docker.prod

--- build.prod.sh
+++ build.prod.sh
@@ -3,6 +3,7 @@
 [ -f .env ] && source .env
 
 # build backend
+docker volume prune -f
 docker-compose -f docker-compose-staging.yml build
 docker-compose -f docker-compose-staging.yml run backend rake assets:precompile
 docker-compose -f docker-compose-staging.yml down
